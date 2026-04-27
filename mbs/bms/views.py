06db22913b2e352efd47dff401aa284ca0c441b3
jateen122 <jatinshresth69email@gmail.com>
2026-04-27 19:32:02 +0545

    Adding login required feature

--- mbs/bms/views.py
+++ mbs/bms/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Book
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
+@login_required(login_url='login/')
 def books(request):
     if request.method == "POST":
         book_name = request.POST.get('book_name')
@@ -27,21 +30,29 @@ def delete_book(request, id):
 
 
 def update_book(request, id):
-    book = get_object_or_404(Book, id=id)
+    queryset = Book.objects.get(id=id)
 
     if request.method == "POST":
-        book.book_name = request.POST.get('book_name')
-        book.book_description = request.POST.get('book_description')
+        book_name = request.POST.get('book_name')
+        book_description = request.POST.get('book_description')
+        book_image = request.FILES.get('book_image')
 
-        # update image only if new one is uploaded
-        if request.FILES.get('book_image'):
-            book.book_image = request.FILES.get('book_image')
+        queryset.book_name = book_name
+        queryset.book_description = book_description
 
-        book.save()
-        return redirect('books')   # better than '/'
-
-    context = {'book': book}
+        if book_image:
+            queryset.book_image = book_image
+        queryset.save()
+        return redirect('books')
+    context = {'book': queryset}
     return render(request, 'updatebooks.html', context)
+
 
 def login_page(request):
     return render(request, 'login.html')
+
+def logout_page(request):
+    pass
+
+def register_page(request):
+    pass
